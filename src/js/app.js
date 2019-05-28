6ce1f3968f6b6cf27d59d1c02d9211457e97d877
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-05-28 09:18:05 +0530

    Moved to src/

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+$("#switchTeacherBtn").click(function(){
+	$("#login").css("display", "none");
+	$("#teacher").css("display", "block");
+});
+
+$("#switchStudentBtn").click(function(){
+	$("#login").css("display", "none");
+	$("#student").css("display", "block");
+})
